94b420ba1a3c79e363dfe311470e73fe0f69c2c1
jinwoo park <swat018@gmail.com>
2022-09-04 12:57:32 +0900

    feat : 클릭카운트뷰모듈 - 스팩1 (의존성 주입 체크)

--- ClickCounter/ClickCountView.js
+++ ClickCounter/ClickCountView.js
@@ -1,6 +1,8 @@
 var App = App || {}
 
 App.ClickCountView = (clickCounter, updateEl) => {
+    if (!clickCounter) throw Error('clickCounter')
+
     return {
         updateView() {
             updateEl.innerHTML = clickCounter.getValue()
--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -6,6 +6,18 @@ describe('App.ClickCountView', ()=> {
         view = App.ClickCountView(clickCounter, updateEl)
     })
 
+    it('clickCounter를 주입하지 않으면 에러를 던진다.', ()=> {
+        const clickCounter = null
+        const updateEl = document.createElement('span')
+
+        const actual = () => App.ClickCountView(clickCounter, updateEl)
+        expect(actual).toThrowError()
+    })
+
+    it('updateEl를 주입하지 않으면 에러를 던진다.', ()=> {
+
+    })
+
     describe('updateView()', ()=> {
         it('ClickCounter의 getValue() 값을 출력한다.', ()=> {
             const counterValue = clickCounter.getValue()
